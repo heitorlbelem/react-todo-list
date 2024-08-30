56c35a1ad561402f2a78e4dbbaa68bdcaf9d3f89
Heitor Belem <heitorlbelem98@gmail.com>
2024-08-30 13:55:09 -0300

    Fix or ignore eslint errors

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,7 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/no-empty-object-type': 'off',
       quotes: ["error", "double"],
       semi: ["error", "always"]
     },
