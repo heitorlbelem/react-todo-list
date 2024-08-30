bc72b3158c822c6cab2525eb67015dbbb5687b03
Heitor Belem <heitorlbelem98@gmail.com>
2024-08-30 10:36:53 -0300

    Adding global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,11 +5,14 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    font-family: 'Inter', sans-serif;
   }
 
   body {
     height: 100vh;
     width: 100%;
+
     background-color: ${props => props.theme["gray-600"]};
     color: ${props => props.theme["gray-300"]};
   }
